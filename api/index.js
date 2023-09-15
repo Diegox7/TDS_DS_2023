d33cccb6cfcd95bfa6f775fb72ab8881a4169e76
diegox7 <diego.g.silva99@gmail.com>
2023-09-15 17:11:10 -0300

    aprendendo sobre api

--- api/index.js
+++ api/index.js
@@ -1,1 +1,35 @@
-console.log("hello world");
+const express = require("express");
+const bodyParse = require('express').bodyParse;
+const app = new express();
+
+app.get("/alunos", (request, response)=>{
+    response.send("alunos");
+});
+
+app.get("/professores", (request, response)=>{
+    response.send("professores");
+});
+
+app.get("/soma", (request, response)=>{
+    var valor1 = 10;
+    var valor2 = 10;
+
+    var resultado = valor1 + valor2;
+
+    response.send(`O resultado da soma é: ${resultado}`);
+});
+
+app.post("/somaParametros", (request, response)=>{
+    const valor1 = request.body.valor1;
+    const valor2 = request.body.valor2;
+
+    const resultado = valor1 + valor2;
+
+    response.send(`O resultado da soma dos parâmetros é: ${resultado}`)
+});
+
+
+
+app.listen(8080, (error)=>{
+        console.log("O servidor está rodando na porta 8080! 👍");
+});
